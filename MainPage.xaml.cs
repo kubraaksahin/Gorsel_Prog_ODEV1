950c23ee3e47d4ca4ce2157e6c0218c270c7c90d
Kübra Akşahin <147938504+kubraaksahin@users.noreply.github.com>
2025-10-15 22:25:09 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnStandardClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("standard");
+        }
+
+        private async void OnScientificClicked(object sender, EventArgs e)
+        {
+
+            await Shell.Current.GoToAsync("scientific");
+        }
+
+    }
+}
